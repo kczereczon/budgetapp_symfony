1ab022e755c1816f178802704d807e21bc6b9c2a
Krzysztof Czereczon <kczereczon@gmail.com>
2023-03-15 21:51:28 +0100

    added table viewa

--- src/Controller/TransactionsController.php
+++ src/Controller/TransactionsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Transaction;
 use App\Form\TransactionFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,20 +12,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class TransactionsController extends AbstractController
 {
     #[Route('/transactions', name: 'app_transactions')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
 
         $form = $this->createForm(TransactionFormType::class);
 
-        $transactions = [];
-
-        foreach (range(start: 0, end:  100) as $int) {
-            $transaction = new Transaction();
-        }
+        $transactions = $entityManager->getRepository(Transaction::class)->findBy(criteria: [], limit: 25);
 
         return $this->render('transactions/index.html.twig', [
             'controller_name' => 'TransactionsController',
             'transactionForm' => $form,
+            'transactions' => $transactions
         ]);
     }
 }
--- src/Entity/Subcategory.php
+++ src/Entity/Subcategory.php
@@ -88,4 +88,8 @@ class Subcategory
 
         return $this;
     }
+
+    public function __toString() {
+        return $this->getName() ?? "";
+    }
 }
